96d9f43ea384d96f8a7857661f8cadaa1682689b
zxz4 <zxz4@msn.com>
2021-11-06 21:45:49 +0800

    Factory

---
+++ Creational/FactoryMethod.cs
@@ -0,0 +1,61 @@
+﻿namespace DesignPattern.Creational
+{
+    /// <summary>
+    /// 工厂方法（Factory Method）
+    /// 定义了一个创建对象的接口，由子类决定具体创建哪个对象。
+    /// </summary>
+    public abstract class FactoryMethod
+    {
+        public interface IProduct
+        {
+            void Behavior();
+        }
+
+
+        /// <summary>
+        /// 这里使用protected来完全隐藏细节
+        /// </summary>
+        protected class DefaultProduct : IProduct
+        {
+            public void Behavior()
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 实现2
+        /// </summary>
+        protected class SpecialProduct : IProduct
+        {
+            public void Behavior()
+            {
+            }
+        }
+
+        /// <summary>
+        /// 与简单工厂不同的是，工厂方法创建对象是由具体的工厂子类实现
+        /// </summary>
+        /// <returns></returns>
+        public abstract IProduct CreateProduct();
+    }
+
+
+
+    public class DefaultProductFactory : FactoryMethod
+    {
+        public override IProduct CreateProduct()
+        {
+            return new DefaultProduct();
+        }
+    }
+
+
+    public class SpecialProductFactory : FactoryMethod
+    {
+        public override IProduct CreateProduct()
+        {
+            return new SpecialProduct();
+        }
+    }
+}
---
+++ Creational/SimpleFactory.cs
@@ -0,0 +1,64 @@
+﻿namespace DesignPattern.Creational
+{
+    /// <summary>
+    /// 简单工厂（Simple Factory）
+    /// 在创建一个对象时不向客户暴露内部细节，并提供一个创建对象的通用接口。
+    /// </summary>
+    public class SimpleFactory
+    {
+        public interface IProduct
+        {
+            void Behavior();
+        }
+
+
+        /// <summary>
+        /// 这里使用protected来完全隐藏细节
+        /// </summary>
+        protected class DefaultProduct : IProduct
+        {
+            public void Behavior()
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 实现2
+        /// </summary>
+        protected class SpecialProduct : IProduct
+        {
+            public void Behavior()
+            {
+            }
+        }
+
+
+        /// <summary>
+        /// 产品类型
+        /// </summary>
+        public enum ProductType
+        {
+            DefaultProduct,
+            SpecialProduct
+        }
+
+        /// <summary>
+        /// 由工厂决定具体创建哪个对象
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public IProduct CreteProduct(ProductType type)
+        {
+            //这样客户端只需知道产品类型而不在需要了解具体子类的细节，从而将具体产品与客户端解耦
+            switch (type)
+            {
+                case ProductType.SpecialProduct:
+                    return new SpecialProduct();
+                case ProductType.DefaultProduct:
+                default:
+                    return new DefaultProduct();
+            }
+        }
+    }
+}
